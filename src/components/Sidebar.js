f84df6f9e638d449c65a470e79d0416fbf13f80b
Marco Laspe <marco@rockiger.com>
2019-01-23 15:01:55 +0100

    read the file directory, filter and sort it

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,87 +1,147 @@
+import fs from 'fs';
+import path from 'path';
+
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FaChevronDown, FaChevronRight } from 'react-icons/fa';
 // import Tree from './Tree';
+import {
+    FocusStyleManager, Classes, Icon, ITreeNode, Position, Tooltip, Tree,
+} from '@blueprintjs/core';
 import { smallTree, tree, blueprintTree } from './treeSampleData.js';
 
-import { FocusStyleManager, Classes, Icon, ITreeNode, Position, Tooltip, Tree } from "@blueprintjs/core";
-FocusStyleManager.onlyShowFocusOnTabs();
 
 import { BASEPATH, EXT } from '../constants';
-//import { sidebar, sidebar__ul } from './index.css';
+
+FocusStyleManager.onlyShowFocusOnTabs();
+// import { sidebar, sidebar__ul } from './index.css';
+
+
+directoryTreeToObj(BASEPATH, (err, results) => {
+    if (err) throw err;
+    console.log(results);
+});
+
 
 export default class Sidebar extends Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-    this.state = { nodes: blueprintTree }
+    constructor(props) {
+        super(props);
+        this.props = props;
+        this.state = { nodes: [] };
 
-    this.handleNodeClick = this.handleNodeClick.bind(this);
-    this.handleNodeCollapse = this.handleNodeCollapse.bind(this);
-    this.handleNodeExpand = this.handleNodeExpand.bind(this);
-  }
-
-  handleNodeClick(nodeData, e) {
-      console.log('handleNodeClick:', nodeData, e);
-      this.setState(this.state)
-  }
-  
-  handleNodeCollapse(nodeData) {
-      console.log('handleNodeCollapse:', nodeData)
-      nodeData.isExpanded = false;
-      this.setState(this.state)
-  }
-  
-  handleNodeExpand(nodeData) {
-      console.log('handleNodeExpand:', nodeData, this.state)
-      nodeData.isExpanded = true;
-      this.setState(this.state)
-  }
-
-  onToggle(node) {
-    const { nodes } = this.state;
-    nodes[node.path].isOpen = !node.isOpen;
-    this.setState({ nodes });
-  }
-
-  render() {
-    return (
-      <div>
-        <Tree
-          contents={this.state.nodes}
-          onNodeClick={this.handleNodeClick}
-          onNodeCollapse={this.handleNodeCollapse}
-          onNodeExpand={this.handleNodeExpand}
-        />
-      </div>
-    );
-  }
-}
-
-
-function hasDirectory(name, list) {
-  for (const e of list) {
-    if (name === e.name && e.type === 'dir') return true;
-  }
-  return false;
-}
-
-function hasFile(name, list) {
-  for (const e of list) {
-    if (name + EXT === e.name) {
-      return true;
+        this.handleNodeClick = this.handleNodeClick.bind(this);
+        this.handleNodeCollapse = this.handleNodeCollapse.bind(this);
+        this.handleNodeExpand = this.handleNodeExpand.bind(this);
     }
-  }
-  return false;
+
+    componentDidMount() {
+        directoryTreeToObj(BASEPATH, (err, results) => {
+            if (err) throw err;
+
+            if (results) this.setState({ nodes: results });
+        });
+    }
+
+    handleNodeClick(nodeData, e) {
+        console.log('handleNodeClick:', nodeData, e);
+        this.setState(this.state);
+    }
+
+    handleNodeCollapse(nodeData) {
+        console.log('handleNodeCollapse:', nodeData);
+        nodeData.isExpanded = false;
+        this.setState(this.state);
+    }
+
+    handleNodeExpand(nodeData) {
+        console.log('handleNodeExpand:', nodeData, this.state);
+        nodeData.isExpanded = true;
+        this.setState(this.state);
+    }
+
+    render() {
+        return (
+            <div>
+            <Tree
+                    contents={this.state.nodes}
+                    onNodeClick={this.handleNodeClick}
+                    onNodeCollapse={this.handleNodeCollapse}
+                    onNodeExpand={this.handleNodeExpand}
+              />
+          </div>
+        );
+    }
 }
 
-function hasChild(el) {
-  if (el.hasOwnProperty('children')) {
-    for (const child of el.children) {
-      if (child.name.endsWith(EXT)) {
-        return true;
-      }
-    }
-  }
-  return false;
+/**
+ * Creates a sorted directory tree with folders and text-files combined
+ * @param {string} dir - the (absolute) path to the directory
+ * @param {function} done - the callback to handle the result (err, result<array>)
+ */
+function directoryTreeToObj(dir, done) {
+    const results = [];
+
+    fs.readdir(dir, (err, list) => {
+        if (err) return done(err);
+
+        let pending = list.length;
+
+        if (!pending) {
+            return done(null, {
+                id: dir,
+                key: dir,
+                label: path.basename(dir),
+                type: 'folder',
+                childNodes: results.sort(sortTree),
+            });
+        }
+
+        list.forEach((file) => {
+            const nFile = path.resolve(dir, file);
+            fs.stat(nFile, (err, stat) => {
+                if (stat && stat.isDirectory()) {
+                    directoryTreeToObj(nFile, (err, res) => {
+                        results.push({
+                            id: nFile,
+                            label: path.basename(nFile),
+                            type: 'folder',
+                            childNodes: res.sort(sortTree),
+                        });
+                        if (!--pending) done(null, results.sort(sortTree));
+                    });
+                } else {
+                    fs.stat(withoutExt(nFile), (err, stat) => {
+                        if (err) {
+                            results.push({
+                                id: nFile,
+                                label: withoutExt(path.basename(nFile)),
+                                type: 'file',
+                            });
+                        }
+                    });
+                    if (!--pending) done(null, results);
+                }
+            });
+        });
+    });
+}
+
+/**
+ * Sorts an array of TreeNodes
+ * https://blueprintjs.com/docs/#core/components/tree.tree-node
+ * @param {ITreeNode} a
+ * @param {ITreeNode} b
+ */
+function sortTree(a, b) {
+    if (a.label < b.label) return -1;
+    if (a.label > b.label) return 1;
+    return 0;
+}
+
+/**
+ * Removes the markdown extension
+ * @param {string} fileName - the filename to remove the extension
+ */
+function withoutExt(fileName) {
+    return fileName.slice(0, -EXT.length);
 }
