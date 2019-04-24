997a54d358234bdd330fd1fff8ec08c8068e94fd
Marco Laspe <marco@rockiger.com>
2019-04-24 06:05:40 +0200

    write test with enzyme

--- src/app.test.js
+++ src/app.test.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-import { Sidebar, NewFileDialog, OpenPageBar } from './app';
+import App, {
+    Sidebar, NewFileDialog, OpenPageBar,
+} from './app';
 
 import { HOMEPATH, DEFAULT_PAGE } from './constants';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('App', () => {
+    it('renders shallow without crashing', () => {
+        const div = document.createElement('div');
+        shallow(
+            <App />,
+        );
+    });
+});
+
+describe('Sidebar', () => {
+    it('renders shallow without crashing', () => {
+        const div = shallow(
+            <Sidebar
+                currentFile=""
+                updateSidebar={false}
+                setCurrentFile={() => {}}
+                toggleNewFileDialog={() => {}}
+                setNewFileDir={() => {}}
+                setUpdateSidebar={() => {}}
+            />,
+        );
+        expect(div.find('.sidebar').length).toBe(1);
+    });
+});
 
 describe('OpenPageBar', () => {
     it('renders without crashing', () => {
@@ -12,7 +43,7 @@ describe('OpenPageBar', () => {
         ReactDOM.render(
             <OpenPageBar
                 isOpen={false}
-                setCurrentFile={HOMEPATH}
+                setCurrentFile={() => {}}
                 setOpenPageBar={() => {}}
             />,
             div,
